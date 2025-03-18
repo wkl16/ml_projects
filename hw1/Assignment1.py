9f6541a7052360c4248846ead14c627df6170273
Matcha <mattm308mttam@gmail.com>
2025-03-18 16:55:04 -0600

    Update Assignment1.py
    Script Should run now

--- hw1/Assignment1.py
+++ hw1/Assignment1.py
@@ -1,17 +1,17 @@
-def run_script(script)
+def run_script(script):
     with open(script, 'r') as file: script_code = file.read()
     exec(script_code, globals())
 
-if __name__ == __main__
+if __name__ == "__main__":
     print("Executing Task 1")
     print("Plotting Perceptron…")
-    run_script(perceptron.py)
+    run_script("perceptron.py")
     print("Plotting Adaline…")
-    run_script(adaline.py)
+    run_script("adaline.py")
     print("Executing Task 3")
     print("Running Multiclass Demo…")
-    run_script(multiclassdemo.py)
+    run_script("multiclassdemo.py")
     print("Executing Task 4")
     print("Plotting Adaline GD, SGD, and Mini-batch SGD")
-    run_script(PlotAdaline.py)
+    run_script("PlotAdaline.py")
     print("Assignment 1 finished")
