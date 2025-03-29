0693672fb3de76c4ed02da4915ca91e49bf473e6
elliejeanjohn <elliejeanjohnson413@gmail.com>
2025-03-28 18:22:03 -0600

    created template run script

---
+++ midterm/midterm.py
@@ -0,0 +1,22 @@
+# copied assignment 1 run script
+# needs to be a executable python file set up so that TA can run it and corroborate results in the report.
+
+# def run_script(script):
+#     with open(script, 'r') as file: script_code = file.read()
+#     exec(script_code, globals())
+
+# if __name__ == "__main__":
+#     print("Executing Task 1")
+#     print("Plotting Perceptron…")
+#     run_script("perceptron.py")
+#     print("Plotting Adaline…")
+#     run_script("adaline.py")
+#     print("Executing Task 3")
+#     print("Running Multiclass Demo…")
+#     run_script("multiclassdemo.py")
+#     print("Executing Task 4")
+#     print("Plotting Parameter Set 1")
+#     run_script("PlotAdaline_Param_Set_1.py")
+#     print("Plotting Parameter Set 2")
+#     run_script("PlotAdaline_Param_Set_2.py")
+#     print("Assignment 1 finished")
