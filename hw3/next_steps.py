ae18684926841d9cbefecc1f945e26f7f64f121c
Matcha <mattm308mttam@gmail.com>
2025-04-03 17:59:42 -0600

    Added next_steps.py
    Template for next Task in Assignment 3. make_pipeline is used to make the requested pipeline as instructed in the assignment. Standard Scaler is used to standardize the data from generate_data() also as requested. The pipeline works as intended and currently prints out a score of 0.966.

---
+++ hw3/next_steps.py
@@ -0,0 +1,12 @@
+from generate_data import generate_data
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = generate_data()
+    
+    # Assuming this is the next step of the assignment here
+    A3_pipe = make_pipeline(StandardScaler(), SVC(random_state=42))
+    print(A3_pipe.fit(X_train, y_train).score(X_test, y_test))
+    
