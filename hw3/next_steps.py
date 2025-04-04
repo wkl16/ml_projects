554bc00fad9b36706bc73e104dea94fee0231f29
Matcha <mattm308mttam@gmail.com>
2025-04-03 19:11:01 -0600

    Update next_steps.py
    Fixed imports.

--- hw3/next_steps.py
+++ hw3/next_steps.py
@@ -1,10 +1,11 @@
-from generate_data import generate_data
+from generate_data import generate_data_numbers
+from generate_data import generate_data_fashion
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 
 if __name__ == "__main__":
-    X_train, X_test, y_train, y_test = generate_data()
+    X_train, X_test, y_train, y_test = generate_data_numbers()
     
     # Assuming this is the next step of the assignment here
     A3_pipe = make_pipeline(StandardScaler(), SVC(random_state=42))
