1ecd47ab8a89cb9b585bfe07d6d7d73a3d444207
tenoriojk <tenoriojk@gmail.com>
2025-05-11 15:13:07 -0600

    Update environment_22.py
    refactored and commented code.

--- final/environment_22.py
+++ final/environment_22.py
@@ -1,14 +1,52 @@
-import numpy as np
-from torch.distributions.constraints import positive
+# Task 2.2 - Building environment
 
+import numpy as np
 
 class Environment:
+    """Environment for reinforcement learning. A 2d numpy array.
+    Parameters
+    ------------
+    map_abstract : 2d numpy array
+    map should be built from map_abstraction class.
+    playerX_pos : int
+    player/agent starting x position.
+    playerY_pos : int
+    player/agent starting y position.
+    goalX_pos : int
+    goals x position.
+    goalY_pos : int
+    goals y position.
+    negative_reward : int
+    penalization value for trying to move into objects or out of the numpy array.
+    neutral_reward : int
+    reward for moving around the environment.
+    goal_reward : int
+    reward for finding the goal.
+    Attributes
+    -----------
+    env_map : 2d numpy array
+    the environment, shown as a map (2d numpy array).
+        Values in the env_map:
+         0 : free move location
+         1 : obstacle
+         2 : agent
+         5 : goal
+         7 : agent and goal when agent is on the goal position
+    cols : int
+    width of numpy array.
+    rows : int
+    height of numpy array.
+    original_map : 2d numpy array
+    starting map, used when starting a new episode.
+    """
     def __init__(self, map_abstract, playerX_pos, playerY_pos, goalX_pos, goalY_pos, negative_reward=-1, neutral_reward=0, goal_reward=1000):
         self.env_map = map_abstract
         self.cols, self.rows = self.env_map.shape
         self.negative_reward = negative_reward
         self.neutral_reward = neutral_reward
         self.goal_reward = goal_reward
+
+        #conditionals to not allow initialization if player or goal position are not correct.
         if not self.validate_start(playerX_pos, playerY_pos):
             raise ValueError("Player must be set in valid position")
         else:
@@ -22,6 +60,17 @@ class Environment:
         self.original_map = self.env_map.copy()
 
     def validate_start(self, X_pos, Y_pos):
+        """Validate user requested starting positions, if valid return true, else return false.
+        Parameters
+        ----------
+        X_pos : int
+        requested x position.
+        Y_pos : int
+        requested y position.
+        Returns
+        -------
+        bool
+        """
         if (Y_pos < 0) or (Y_pos > (self.rows - 1)):
             return False
         if (X_pos < 0) or (X_pos > (self.cols - 1)):
@@ -32,16 +81,40 @@ class Environment:
             return True
 
     def where_am_i(self, state):
+        """Tells where the agent is on the map.
+        Parameters
+        ----------
+        state : 2d numpy array
+        current environment.
+        Returns
+        -------
+        array : numpy array
+        gives agent position.
+        """
         location = np.argwhere((state == 2) | (state == 7))
         return location[0]
 
     def interaction(self, state, action):
-        reward = 0
+        """Takes in current environment state and requested agent action
+        and returns updated state and reward for action.
+        Parameters
+        ----------
+        state : 2d numpy array
+        current environment.
+        Returns
+        -------
+        env_map : 2d numpy array
+        state of the environment after requested agent action.
+        reward : int
+        reward for requested action.
+        """
+        #copy initial state and find agent position.
         new_state = state
         agent_location = self.where_am_i(state)
         agent_row = agent_location[0]
         agent_col = agent_location[1]
-        
+
+        #get requested agent position.
         if action == "left":
             requestX_pos = agent_col - 1
             requestY_pos = agent_row
@@ -58,6 +131,7 @@ class Environment:
             requestX_pos = agent_col
             requestY_pos = agent_row
 
+        #check if move is valid, update agent position, and reward.
         if not self.validate_move(requestX_pos, requestY_pos):
             reward = self.negative_reward
         else:
@@ -70,10 +144,24 @@ class Environment:
                 new_state[requestY_pos, requestX_pos] = 2
                 reward = self.neutral_reward
 
+        #update environment with the latest state.
         self.env_map = new_state
+
+        #return new state and reward.
         return self.env_map, reward
 
     def validate_move(self, requestX_pos, requestY_pos):
+        """Validate agent requested position, if valid return true, else return false.
+        Parameters
+        ----------
+        X_pos : int
+        requested x position.
+        Y_pos : int
+        requested y position.
+        Returns
+        -------
+        bool
+        """
         if (requestY_pos < 0) or (requestY_pos > (self.rows - 1)):
             return False
         if (requestX_pos < 0) or (requestX_pos > (self.cols - 1)):
@@ -84,10 +172,16 @@ class Environment:
             return True
 
     def env_print(self):
-        #print(f"# rows: {self.rows}")
-        #print(f"# cols: {self.cols}")
+        """Helper function that prints environment map.
+        """
         print(self.env_map)
 
     def reset(self):
+        """Reset environment for next .
+        Returns
+        -------
+        env_map : 2d numpy array
+        sends the original starting map.
+        """
         self.env_map = self.original_map.copy()
         return self.env_map.copy()
